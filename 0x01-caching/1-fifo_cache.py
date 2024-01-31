3c4ffb93836040e03b440f78f4ed1242271d0bac
laila22haz <hazmirlaila316@gmail.com>
2024-02-01 00:20:19 +0100

    HERE WE GOO

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,6 +6,7 @@ from base_caching import BaseCaching
 class FIFOCache(BaseCaching):
     """class FIFO"""
     def __init__(self):
+        """init fuction"""
         super().__init__()
         self.key_idexes = []
 
