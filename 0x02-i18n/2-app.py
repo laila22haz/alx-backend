79790cf5e4fdea320856ce51e802845843d6ae2b
laila22haz <hazmirlaila316@gmail.com>
2024-02-06 23:39:46 +0100

    GET_LOCALE

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,7 +21,7 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """get_local function"""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(["LANGUAGES"])
 
 
 @app.route('/', strict_slashes=False)
