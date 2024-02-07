c08e2c548e4d135bad3e786a53d835feb81ed0ea
laila22haz <hazmirlaila316@gmail.com>
2024-02-07 13:55:33 +0100

    app

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get_local function"""
+    return request.accept_languages.best_match(["LANGUAGES"])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return render_template("3-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
